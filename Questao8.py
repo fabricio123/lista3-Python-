3aaedbc12c3a66e41faa65f8aa31540597485a28
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-24 20:06:51 -0300

    Create Questao8.py
    Algoritmo  que utiliza  de laço  de repetição para realizar a soma e a media.

---
+++ Questao8.py
@@ -0,0 +1,13 @@
+
+c = 0
+media = 0
+soma = 0
+
+while (c <=5):
+    n = float(input('digite um numero :'))
+    
+    soma = soma + n
+    media = soma/5
+    c = c+1
+print( 'a soma dos números  é: ', soma)
+print(' a media é dos números é:', media)
